4c4638d593018498d7e14f128203bc07f77c211d
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2021-04-27 18:14:22 -0400

    Update EditLocation.h

--- EditLocation.h
+++ EditLocation.h
@@ -7,4 +7,8 @@
 
 int EditChest(std::fstream& fptr, ACTR chest, uint8_t item_id, int offset);
 
+int EditActor(std::fstream& fptr, ACTR actor, uint8_t item_id, int offset);
+
+int EditScob(std::fstream& fptr, SCOB scob, uint8_t item_id, int offset);
+
 #endif
