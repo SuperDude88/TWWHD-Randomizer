1df901ed22464c21f16d3ccd108f8ae9df147e32
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2021-05-04 19:45:20 -0400

    Added EditEvent definition

--- EditLocation.hpp
+++ EditLocation.hpp
@@ -10,3 +10,5 @@ int EditActor(std::fstream& fptr, ACTR actor, uint8_t item_id, int offset);
 int EditScob(std::fstream& fptr, SCOB scob, uint8_t item_id, int offset);
 
 int EditRPX(std::fstream& fptr, uint8_t item_id, int offset);
+
+int EditEvent(std::fstream& fptr, uint8_t item_id, int offset, int nameoffset);
