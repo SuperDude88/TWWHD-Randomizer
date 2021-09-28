6bbd2022fe6a9ddba6ab9450c438e19ccb46a044
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2021-09-28 17:58:55 -0400

    Delete Location.hpp

--- Location.hpp
+++
@@ -1,9 +0,0 @@
-#define Location_H
-#pragma once
-
-#include "Structs.hpp"
-#include <vector>
-
-LocationLists FindPossibleProgressLocations(std::vector<Location> Locations, std::unordered_set<std::string> settings);
-
-int WriteLocations(LocationLists list, std::fstream fptr);
