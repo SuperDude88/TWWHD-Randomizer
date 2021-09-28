e9a07e6b0a3defcea8f35a1763542a24d574fc3f
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2021-09-28 17:59:23 -0400

    Delete Parse.hpp

--- Parse.hpp
+++
@@ -1,9 +0,0 @@
-#define Parse_H
-#pragma once
-
-#include <vector>
-#include "Structs.hpp"
-
-std::vector<Location> ParseLocations(std::string LocationsPath);
-
-std::vector<Macro> ParseMacros(std::string MacrosPath);
