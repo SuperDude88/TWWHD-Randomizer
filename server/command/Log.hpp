9309b34fa0e1df078bfb049230551b4497acce72
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2022-07-15 12:43:58 -0400

    Logging updates
    - Add LogInfo class that stores basic seed info
    - Add error log
    - Add timestamps to logging
    - Allow saving/loading settings from a file

--- server/command/Log.hpp
+++ server/command/Log.hpp
@@ -1,14 +1,81 @@
 #pragma once
 
+#include <ratio>
 #include <string>
 #include <fstream>
 #include <typeinfo>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+#include "../../seedgen/config.hpp"
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #define __FILENAME__ (&__FILE__[SOURCE_PATH_SIZE])
 
 
+
+//make these thread safe?
+
+class ProgramTime {
+public: 
+    using TimePoint_t = std::chrono::time_point<std::chrono::system_clock>;
+
+    ProgramTime(const ProgramTime&) = delete;
+    ProgramTime& operator=(const ProgramTime&) = delete;
+private:
+    const TimePoint_t openTime;
+
+    ProgramTime();
+    ~ProgramTime();
+
+    static ProgramTime& getInstance();
+
+public:
+    static TimePoint_t::duration getElapsedTime() { return std::chrono::system_clock::now() - getOpenedTime(); }
+    static TimePoint_t getOpenedTime() { return getInstance().openTime; }
+};
+
+inline std::string formatTime(const ProgramTime::TimePoint_t::duration& duration_) {
+    using namespace std::chrono;
+    ProgramTime::TimePoint_t::duration duration = duration_;
+    std::stringstream ret;
+    ret <<  std::setfill('0');
+
+    hours hr = duration_cast<hours>(duration);
+    ret << std::setw(2) << hr.count() << ":";
+    duration -= hr;
+    minutes min = duration_cast<minutes>(duration);
+    ret << std::setw(2) << min.count() << ":";
+    duration -= min;
+    seconds sec = duration_cast<seconds>(duration);
+    ret << std::setw(2) << sec.count() << ".";
+    duration -= sec;
+    milliseconds ms = duration_cast<milliseconds>(duration);
+    ret << std::setw(3) << ms.count();
+
+    return ret.str();
+}
+
+class LogInfo {
+private:
+    Config config;
+    std::string seedHash;
+
+    LogInfo();
+    ~LogInfo();
+
+    static LogInfo& getInstance();
+public:
+    LogInfo(const LogInfo&) = delete;
+    LogInfo& operator=(const LogInfo&) = delete;
+    
+    static void setConfig(const Config& config_) { getInstance().config = config_; }
+    static void setSeedHash(const std::string& seedHash_) { getInstance().seedHash = seedHash_; }
+    static const Config& getConfig();
+    static const std::string& getSeedHash();
+};
 
 class BasicLog {
 private:
@@ -21,18 +88,31 @@ public:
     BasicLog& operator=(const BasicLog&) = delete;
 
     static BasicLog& getInstance();
-    void logBasicInfo(const std::string& seed);
     void log(const std::string& msg = "");
 };
 
-#define LOG_ERR_AND_RETURN(error) {\
-    BasicLog::getInstance().log(std::string("Encountered " #error " on line " TOSTRING(__LINE__) " of ") + __FILENAME__); \
+class ErrorLog {
+private:
+    std::ofstream output;
+
+    ErrorLog();
+    ~ErrorLog();
+public:
+    ErrorLog(const ErrorLog&) = delete;
+    ErrorLog& operator=(const ErrorLog&) = delete;
+
+    static ErrorLog& getInstance();
+    void log(const std::string& msg = "");
+};
+
+#define LOG_ERR_AND_RETURN(error) { \
+    ErrorLog::getInstance().log(std::string("Encountered " #error " on line " TOSTRING(__LINE__) " of ") + __FILENAME__); \
     return error; \
 }
 
-#define LOG_AND_RETURN_IF_ERR(func) { /*TODO: maybe make version that returns bool*/ \
+#define LOG_AND_RETURN_IF_ERR(func) { \
     if(const auto error = func; error != decltype(error)::NONE) {\
-        BasicLog::getInstance().log(std::string("Encountered error on line " TOSTRING(__LINE__) " of ") + __FILENAME__); \
+        ErrorLog::getInstance().log(std::string("Encountered error on line " TOSTRING(__LINE__) " of ") + __FILENAME__); \
         return error;  \
     } \
 }
@@ -50,7 +130,6 @@ public:
     DebugLog& operator=(const DebugLog&) = delete;
 
     static DebugLog& getInstance();
-    void logBasicInfo(const std::string& seed);
     void log(const std::string& msg = "");
 };
 
