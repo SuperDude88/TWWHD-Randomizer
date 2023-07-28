ef829c0e833018b528c3ff9df11f1e1472ac8387
gymnast86 <stock.joann@gmail.com>
2023-07-28 15:19:47 -0700

    add color masks to qrc file

--- gui/generate_qrc_file.py
+++ gui/generate_qrc_file.py
@@ -4,7 +4,7 @@
 import os
 
 def main():
-    directories = ['../logic/data', '../asm/patch_diffs', '../assets', '../assets/tracker']
+    directories = ['../logic/data', '../asm/patch_diffs', '../assets', '../assets/tracker', '../assets/link preview', '../assets/link color masks']
     individual_files = ['../asm/custom_symbols.yaml']
     with open('data.qrc', 'w') as qrc:
         qrc.write('<RCC>\n')
