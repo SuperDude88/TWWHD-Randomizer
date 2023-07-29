e246978c77d6dcfd606716677f3c96d20d9ec6fe
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2023-07-29 01:36:50 -0400

    Combine Version and Hash
    - Include commit hash in version number for non-release builds
    - Support release tags passed by command line
    - Support passing extra cmake args to docker build

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -69,7 +69,7 @@ MainWindow::MainWindow(QWidget *parent)
     load_locations();
     load_config_into_ui();
     encounteredError = false;
-    defaultWindowTitle = "Wind Waker HD Randomizer " RANDOMIZER_VERSION "-" COMMIT_HASH;
+    defaultWindowTitle = "Wind Waker HD Randomizer " RANDOMIZER_VERSION;
     this->setWindowTitle(defaultWindowTitle.c_str());
     update_option_description_text();
     currentPermalink = ui->permalink->text();
