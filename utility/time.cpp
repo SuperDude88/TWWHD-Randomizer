e97195d7464b01d67a728e80ab80e3d3d99b3df6
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2023-07-30 19:38:34 -0400

    Various Fixes
    - Warn about mismatched rando version rather than error
    - Fix error in Hints.cpp when building with ENABLE_DEBUG
    - Use old time code when std::format isn't available

--- utility/time.cpp
+++ utility/time.cpp
@@ -1,6 +1,5 @@
 #include "time.hpp"
 
-#include <format>
 
 using namespace std::chrono;
 using namespace std::literals::chrono_literals;
@@ -8,25 +7,8 @@ using namespace std::literals::chrono_literals;
 
 
 ProgramTime::ProgramTime() :
-    openTime(current_zone(), system_clock::now())
+    openTime(system_clock::now())
 {}
-
-ProgramTime::TimePoint_t ProgramTime::getOpenedTime() {
-    return getInstance().openTime;
-}
-
-ProgramTime::TimePoint_t::duration ProgramTime::getElapsedTime() {
-    return system_clock::now() - getOpenedTime().get_sys_time();
-}
-
-std::string ProgramTime::getDateStr() {
-    const auto& opened = getOpenedTime();
-    return std::format("{0:%a %b %d %Y %I:%M:%S %p} {1:%Z%n}", round<seconds>(opened.get_local_time()), opened); //TODO: fix wii u timezone being wrong
-}
-
-std::string ProgramTime::getTimeStr() {
-    return std::format("{:%T}", round<milliseconds>(getElapsedTime()));
-}
 
 ProgramTime::~ProgramTime() {}
 
@@ -34,5 +16,61 @@ ProgramTime& ProgramTime::getInstance() {
     static ProgramTime s_Instance;
     return s_Instance;
 }
+
+ProgramTime::TimePoint_t ProgramTime::getOpenedTime() {
+    return getInstance().openTime;
+}
+
+#ifdef USE_CXX_20_TIME
+    #include <format>
+
+    ProgramTime::TimePoint_t::duration ProgramTime::getElapsedTime() {
+        return system_clock::now() - getOpenedTime().get_sys_time();
+    }
+
+    std::string ProgramTime::getDateStr() {
+        const zoned_time opened(current_zone(), getOpenedTime());
+        return std::format("{0:%a %b %d %Y %I:%M:%S %p} {1:%Z%n}", round<seconds>(opened.get_local_time()), opened); //TODO: fix wii u timezone being wrong
+    }
+
+    std::string ProgramTime::getTimeStr() {
+        return std::format("{:%T}", round<milliseconds>(getElapsedTime()));
+    }
+#else
+    #include <sstream>
+    #include <mutex>
+
+    ProgramTime::TimePoint_t::duration ProgramTime::getElapsedTime() {
+        return system_clock::now() - getOpenedTime();
+    }
+
+    std::string ProgramTime::getDateStr() {
+        const time_t point = system_clock::to_time_t(ProgramTime::getOpenedTime());
+
+        static std::mutex localtimeMut; //std::ctime is not thread safe
+        std::unique_lock<std::mutex> lock(localtimeMut);
+        return std::ctime(&point); //time string ends with \n
+    }
+
+    std::string ProgramTime::getTimeStr() {
+        TimePoint_t::duration duration = getElapsedTime();
+        std::stringstream ret;
+        ret <<  std::setfill('0');
+
+        const hours hr = duration_cast<hours>(duration);
+        ret << std::setw(2) << hr.count() << ":";
+        duration -= hr;
+        const minutes min = duration_cast<minutes>(duration);
+        ret << std::setw(2) << min.count() << ":";
+        duration -= min;
+        const seconds sec = duration_cast<seconds>(duration);
+        ret << std::setw(2) << sec.count() << ".";
+        duration -= sec;
+        const milliseconds ms = duration_cast<milliseconds>(duration);
+        ret << std::setw(3) << ms.count();
+
+        return ret.str();
+    }
+#endif
 
 static const ProgramTime& temp = ProgramTime::getInstance(); //inaccessible global to create instance when program starts
