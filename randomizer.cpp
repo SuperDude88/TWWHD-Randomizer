f82705146fdcd905d433a0718cd9d60543818d0c
gymnast86 <stock.joann@gmail.com>
2023-09-08 14:01:52 -0700

    add seed hash to console SeedMenu

--- randomizer.cpp
+++ randomizer.cpp
@@ -37,8 +37,6 @@
 #include <platform/gui/InstallMenu.hpp>
 #endif
 
-#define SEED_KEY "SEED KEY TEST"
-
 RandoSession g_session; //declared outside of class for extern stuff
 
 #define FILETYPE_ERROR_CHECK(func) {  \
