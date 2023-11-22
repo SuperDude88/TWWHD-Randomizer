95ec18921d8892283d86bbf67208fd6f7e6a4dc6
gymnast86 <stock.joann@gmail.com>
2023-11-22 10:58:49 -0700

    update desktop app for new model stuff

--- customizer/model.hpp
+++ customizer/model.hpp
@@ -30,7 +30,6 @@ public:
 
 class CustomModel {
 private:
-    std::string modelName;
     std::filesystem::path folder;
 
     std::vector<ColorPreset> presets;
@@ -39,18 +38,23 @@ private:
 
 public:
     bool casual;
+    std::string modelName;
 
     const ColorMap_t& getDefaultColorsMap() const;
     const ColorMap_t& getSetColorsMap() const;
     const std::vector<ColorPreset>& getPresets() const;
+    const ColorPreset& getDefaultPreset() const;
     const std::string& getColor(const std::string& name_) const;
     void setColor(const std::string& name_, const std::string& color_);
     void resetColors();
 
     void loadPreset(const size_t& idx);
+    void loadPreset(const std::string& name, const bool& both = true);
     void randomizeOrderly();
     void randomizeChaotically();
 
-    ModelError loadFromFolder(const std::string& name_);
+    ModelError loadFromFolder();
     ModelError applyModel() const;
 };
+
+std::string errorToName(const ModelError& err);
