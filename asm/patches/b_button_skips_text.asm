b6e285cee037ff7611d48745580f94ffc0c6f2f7
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2024-01-25 15:33:24 -0500

    Letter Scrolling
    - Allow holding up or down to scroll through a letter

--- asm/patches/b_button_skips_text.asm
+++ asm/patches/b_button_skips_text.asm
@@ -5,19 +5,22 @@
     lwz r10, 0x50(r8)
     
 .org 0x026fcf18
-    lwz r8,0x50(r30)
+    lwz r8, 0x50(r30)
     
 .org 0x026fd290
-    lwz r0,0x50(r30)
+    lwz r0, 0x50(r30)
     
 .org 0x026fd3dc
-    lwz r12,0x50(r11)
+    lwz r12, 0x50(r11)
     
 .org 0x026fd9b8
-    lwz r9,0x50(r30)
+    lwz r9, 0x50(r30)
     
 .org 0x026fdb40
-    lwz r9,0x50(r30)
+    lwz r9, 0x50(r30)
     
 .org 0x026fe818
-    lwz r8,0x50(r30)
+    lwz r8, 0x50(r30)
+
+.org 0x026ba644
+    lwz r0, 0x50(r3)
