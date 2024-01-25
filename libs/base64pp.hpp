78d8b80c157f389237d9a5c062188ac977f8d064
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2024-01-25 11:48:51 -0500

    More Submodules
    - Move AES and base64 libraries to submodules

--- libs/base64pp.hpp
+++ libs/base64pp.hpp
@@ -1,43 +1,28 @@
-// MIT License
-//
-// Copyright (c) 2022 - Matheus Gomes
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #pragma once
 
-#include <cstdint>
-#include <optional>
-#include <span>
-#include <string>
-#include <string_view>
-#include <vector>
+#include "./base64pp/base64pp/include/base64pp/base64pp.h"
 
-namespace base64pp
-{
-    std::string encode(std::span<std::uint8_t const> const input);
+// extra wrappers for convenience
+inline std::string b64_encode(const std::string& asciiStr) {
+    std::vector<uint8_t> permalinkBytes = {};
+    for(const char& ch : asciiStr)
+    {
+        permalinkBytes.push_back(ch);
+    }
 
-    std::optional<std::vector<std::uint8_t>> decode(
-        std::string_view const encoded_str);
-} // namespace base64pp
+    std::span<const uint8_t> span(permalinkBytes.begin(), permalinkBytes.end());
 
-// Extra wrappers added for this project
-std::string b64_encode(std::string& asciiStr);
+    return base64pp::encode(span);
+}
 
-std::string b64_decode(std::string& b64Str);
+inline std::string b64_decode(const std::string& b64Str) {
+    const auto optional = base64pp::decode(b64Str);
+    if(!optional.has_value())
+    {
+        // Return an empty string if there was an error
+        return "";
+    }
+
+    const auto bytes = optional.value();
+    return std::string(bytes.begin(), bytes.end());
+}
